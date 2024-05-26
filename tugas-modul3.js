cb43a2b4bf40e78549a7ae680d16db77dc126b42
Elen Debina <112352262+elendeee@users.noreply.github.com>
2024-05-27 00:24:38 +0700

    Add files via upload

---
+++ tugas-modul3.js
@@ -0,0 +1,28 @@
+let currentSlide = 0;
+
+function showSlide(index) {
+    const slides = document.querySelectorAll('.carousel-slide');
+    if (index >= slides.length) {
+        currentSlide = 0;
+    } else if (index < 0) {
+        currentSlide = slides.length - 1;
+    } else {
+        currentSlide = index;
+    }
+    const newTransform = `translateX(-${currentSlide * 100}%)`;
+    slides.forEach(slide => {
+        slide.style.transform = newTransform;
+    });
+}
+
+function nextSlide() {
+    showSlide(currentSlide + 1);
+}
+
+function prevSlide() {
+    showSlide(currentSlide - 1);
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    showSlide(currentSlide);
+});
